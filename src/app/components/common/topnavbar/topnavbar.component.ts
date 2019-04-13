3bb47e42e823229f84037c75f178d099e0528235
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-04-13 12:49:41 +0200

    Finalizada redirección automática según estado de login

--- src/app/components/common/topnavbar/topnavbar.component.ts
+++ src/app/components/common/topnavbar/topnavbar.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {smoothlyMenu} from '../../../app.helpers';
 import {Router} from '@angular/router';
 import {USERDATA} from '../../../utiles/constants.interface';
-import Swal from 'sweetalert2'
 
 declare var jQuery:any;
 
@@ -22,12 +21,6 @@ export class TopNavbarComponent {
 
   public logout() {
     localStorage.removeItem(USERDATA);
-    // this.router.navigate(['']);
-    Swal.fire({
-      title: 'Error!',
-      text: 'Do you want to continue',
-      type: 'error',
-      confirmButtonText: 'Cool'
-    });
+    this.router.navigate(['']);
   }
 }
