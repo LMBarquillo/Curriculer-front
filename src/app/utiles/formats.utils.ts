9a94e8f2fafd3494bc95e295a59f1fea68478f7b
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-04-30 21:31:06 +0200

    Finalizado formato de datos inexistentes en el profile

--- src/app/utiles/formats.utils.ts
+++ src/app/utiles/formats.utils.ts
@@ -2,6 +2,8 @@ import {zeroFill} from 'ngx-bootstrap/bs-moment/utils';
 
 export class Formats {
   public static formatDate(utc: string): string {
+    if(utc == null) return "";
+
     let date = new Date(utc);
     return zeroFill(date.getDate(), 2) + '/' + zeroFill(date.getMonth() + 1, 2) + '/' + date.getFullYear();
   }
