2085acd967474fe3924dc28cc38ac8fe483b84aa
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-05-01 11:43:16 +0200

    Mostrar formación en su apartado. Añadido toolbar con botón de añadir. Mostrar avisos si no tiene.

--- src/app/views/training/training.component.ts
+++ src/app/views/training/training.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {TrainingsService} from '../../services/trainings.service';
+import {TrainingModel} from '../../models/training.model';
+import {Swal} from '../../utiles/swal.utils';
+import {Utilities} from '../../utiles/utilities.utils';
 
 @Component({
   selector: 'app-training',
@@ -6,10 +10,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./training.component.scss']
 })
 export class TrainingComponent implements OnInit {
+  public trainings: TrainingModel[] = [];
 
-  constructor() { }
+  constructor(private trainingService: TrainingsService) { }
 
   ngOnInit() {
+    Swal.buildSwallWithoutButtons('Cargando', 'Obteniendo datos. Por favor, espere<br/><i class="fa fa-spinner rotating"></i>', 'info');
+    this.trainingService.getTrainings().subscribe(
+      trainings => {
+        this.trainings = trainings.sort((a, b) => Utilities.compare(a.promotion, b.promotion, true));
+        Swal.close();
+      }, err => {
+        console.log(err);
+        Swal.buildSwalWithoutCancel('Error', 'No se pudo obtener la formación del usuario.', 'error');
+      }
+    );
   }
 
 }
