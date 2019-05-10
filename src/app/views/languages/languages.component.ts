6a2aaf2566bb66f1c7e5246299a1023ad8dab496
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-05-10 19:15:53 +0200

    Mostrar fichas de idiomas

--- src/app/views/languages/languages.component.ts
+++ src/app/views/languages/languages.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {LanguageModel} from '../../models/language.model';
+import {Swal} from '../../utiles/swal.utils';
+import {LanguagesService} from '../../services/languages.service';
 
 @Component({
   selector: 'app-languages',
@@ -6,10 +10,54 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./languages.component.scss']
 })
 export class LanguagesComponent implements OnInit {
+  public languages: LanguageModel[] = [];
+  public formModal: boolean = false;
+  public formGroup: FormGroup;
+  public editing: number;
 
-  constructor() { }
+  constructor(private languageService: LanguagesService) { }
 
   ngOnInit() {
+    Swal.buildSwallWithoutButtons('Cargando', 'Obteniendo datos. Por favor, espere<br/><i class="fa fa-spinner rotating"></i>', 'info');
+    this.languageService.getLanguages().subscribe(
+      languages => {
+        this.languages = languages;
+        this.resetFormGroup();
+        Swal.close();
+      }, err => {
+        console.log(err);
+        Swal.buildSwalWithoutCancel('Error', 'No se pudo obtener la formación del usuario.', 'error');
+      }
+    );
   }
 
+  public addLanguage(): void {
+    this.resetFormGroup();
+    this.formModal = true;
+  }
+
+  public editLanguage(language: LanguageModel): void {
+    this.formGroup = new FormGroup(
+      {
+
+      }
+    );
+    this.editing = language.id;
+    this.formModal = true;
+  }
+
+  public deleteLanguage(language: LanguageModel): void {
+
+  }
+
+
+
+  public resetFormGroup(): void {
+    this.formGroup = new FormGroup(
+      {
+
+      }
+    );
+    this.editing = 0;
+  }
 }
